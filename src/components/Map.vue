26f78a57a7bb1c03b97b667343090b670db51d4e
Michael Petrosyan <mikpetr95@gmail.com>
2018-02-09 10:57:34 +0400

    Created Map component

---
+++ src/components/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="map"></div>
+</template>
+
+<script>
+import GoogleMapsLoader from 'google-maps'
+
+export default {
+  name: 'Map',
+  mounted () {
+    let map
+    let center
+
+    const lat = 40.1808534
+    const lng = 44.5188555
+    const zoomLevel = 14
+
+    GoogleMapsLoader.KEY = 'AIzaSyCwAp6FSvCk1sKaPSQJgThEEGM134-klvU'
+
+    GoogleMapsLoader.load(google => {
+      center = new google.maps.LatLng(lat, lng)
+
+      map = new google.maps.Map(this.$el, {
+        zoom: zoomLevel,
+        center: center
+      })
+    })
+
+    console.log(map)
+
+    GoogleMapsLoader.onLoad(() => {
+      console.log('Map initialized')
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map {
+  width: 800px;
+  height: 400px;
+}
+</style>
