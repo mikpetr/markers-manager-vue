921538c16022220980dea10cbd0c78d15cadb135
Michael Petrosyan <mikpetr95@gmail.com>
2018-02-11 11:35:53 +0400

    Added search functionality

--- src/components/PlacesList.vue
+++ src/components/PlacesList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h2>Places List</h2>
+    <div>
+      Search: <input v-model="filterText" type="text" />
+    </div>
     <ul>
-      <li v-for="place in store.getPlaces()"
+      <li v-for="place in filteredPlaces"
           v-bind:key="place.id"
           v-on:click="showPlace(place)">
         {{place.name}}
@@ -18,7 +21,30 @@ export default {
   name: 'PlacesList',
   data () {
     return {
-      store
+      store,
+      filterText: null
+    }
+  },
+  computed: {
+    filteredPlaces () {
+      if (!this.filterText) {
+        return this.store.getPlaces()
+      }
+
+      return this.store.getPlaces().filter(place => {
+        return place.name.indexOf(this.filterText) > -1
+      }).sort((a, b) => {
+        let aPos = a.name.indexOf(this.filterText)
+        let bPos = b.name.indexOf(this.filterText)
+
+        if (aPos < bPos) {
+          return -1
+        } else if (aPos > bPos) {
+          return 1
+        } else {
+          return 0
+        }
+      })
     }
   },
   methods: {
